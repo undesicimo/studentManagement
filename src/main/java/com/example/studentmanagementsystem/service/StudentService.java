6159d71e1184b83479989eecb2888836a9b3e102
undesicimo <a0ngel03@gmail.com>
2022-11-16 16:56:37 +0900

    first commit

---
+++ src/main/java/com/example/studentmanagementsystem/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.studentmanagementsystem.service;
+
+import java.util.List;
+
+import com.example.studentmanagementsystem.entity.Student;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+    Student updateStudent(Student student);
+    
+    void deleteStudentById(Long id);
+}
